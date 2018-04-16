f7cb451fd5e88c91715d975696ea4423f4bf174b
Marcos <vmvwebworks@gmail.com>
2018-04-16 23:13:49 +0200

    smart listing, wip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   include SmartListing::Helper::ControllerExtensions
   helper  SmartListing::Helper
   def index
-    smart_listing_create :users, User.all, partial: "shared/users_listing"
+    smart_listing_create :users, User.all, partial: "shared/users_listing", page_sizes: [1]
 
   end
 end
