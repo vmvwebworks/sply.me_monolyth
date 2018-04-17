3dad3ee33939634ec8fdef54dcb1cf971cd3a226
Marcos <vmvwebworks@gmail.com>
2018-04-17 22:06:10 +0200

    devise register forms & styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   include SmartListing::Helper::ControllerExtensions
   helper  SmartListing::Helper
   def index
-    smart_listing_create :users, User.all, partial: "shared/users_listing", page_sizes: [1]
-
+    smart_listing_create :users, User.all, partial: "shared/users_listing", page_sizes: [4]
   end
 end
